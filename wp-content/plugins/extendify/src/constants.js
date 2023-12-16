63a9309e639adcbcfbcc6b5cae61171d56e58491
unknown <nicholaskirui6@gmail.com>
2023-12-17 02:42:49 +0300

    sql

--- wp-content/plugins/extendify/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const PATTERNS_HOST = 'https://patterns.extendify.com';
-// export const PATTERNS_HOST = 'http://localhost:3000'
-export const KB_HOST = 'https://kb.extendify.com';
-// export const KB_HOST = 'http://localhost:3000';
